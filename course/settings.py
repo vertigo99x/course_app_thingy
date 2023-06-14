63fae23fae3b2e4c4b11430a5cf5c60185d7ef5b
vertigo99x <83613166+vertigo99x@users.noreply.github.com>
2023-06-14 06:00:23 +0100

    Update settings.py

--- course/settings.py
+++ course/settings.py
@@ -121,8 +121,8 @@ LOGIN_URL = "login"
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
-
+#STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATIC_ROOT = os.path.join(BASE_DIR,"static")
 
 
 MEDIA_URL = '/media/'
